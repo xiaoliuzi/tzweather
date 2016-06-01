25d80f3117362a3f8e7b1829cf61727299e2db4e
xiaoliuzi <qingpengliu@qq.com>
2016-06-01 20:07:23 +0800

    Create second Activity

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -1,13 +1,32 @@
 package com.mycompany.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+
+
 
 public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
+
+
